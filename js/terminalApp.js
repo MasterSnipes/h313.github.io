31656332b6700c7f8b5a3ec250afac005d0efe1e
Harry <arandomawesomegeek@gmail.com>
2016-09-27 21:20:36 -0400

    screwed up brackets again

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -5,17 +5,16 @@ terminalApp.controller('TerminalController', function TerminalController($scope,
   $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
   $scope.update = function(keyEvent) {
     if (keyEvent.which === 13) {
-        if($scope.command === "help") {
-          $scope.output = $sce.trustAsHtml('<p><span class="green">ls</span></p>');
-        }
-        if($scope.command === "ls") {
-          $scope.output = $sce.trustAsHtml('<p>projects</p>');
-        }
-        if($scope.command === "cat projects") {
-          $scope.output = $sce.trustAsHtml('<p><a class="red" href="https://h313.github.io/KarlMarkov/">Karl Markov</a></p><p><a class="red" href="https://h313.github.io/SirMarkov/">Sir Markov</a></p>');
-        }
-        $scope.command = "";
+      if($scope.command === "help") {
+        $scope.output = $sce.trustAsHtml('<p><span class="green">ls</span></p>');
       }
+      if($scope.command === "ls") {
+        $scope.output = $sce.trustAsHtml('<p>projects</p>');
+      }
+      if($scope.command === "cat projects") {
+        $scope.output = $sce.trustAsHtml('<p><a class="red" href="https://h313.github.io/KarlMarkov/">Karl Markov</a></p><p><a class="red" href="https://h313.github.io/SirMarkov/">Sir Markov</a></p>');
+      }
+      $scope.command = "";
     }
   };
 });

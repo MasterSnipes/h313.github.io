508e37977584240830cf06fec9b72ce6330d5aef
Harry <arandomawesomegeek@gmail.com>
2016-09-27 21:15:29 -0400

    fixed

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -3,7 +3,7 @@ var terminalApp = angular.module('terminalApp', []);
 terminalApp.controller('TerminalController', function TerminalController($scope, $sce){
   $scope.command = "help";
   $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
-  $scope.update = function() {
+  $scope.update = function(keyEvent) {
     if (keyEvent.which === 13) {
       if($scope.command == "help")
       $scope.output = $sce.trustAsHtml('<p><span class="green">ls</span></p>');

5f5bc6e37d5bb42dfa048697f1031a89a598d437
Harry <arandomawesomegeek@gmail.com>
2016-09-28 10:47:40 -0400

    Update terminalApp.js

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -19,17 +19,3 @@ terminalApp.controller('TerminalController', function TerminalController($scope,
     $scope.command = "";
   };
 });
-
-app.directive('myEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.myEnter);
-                });
-
-                event.preventDefault();
-            }
-        });
-    };
-});

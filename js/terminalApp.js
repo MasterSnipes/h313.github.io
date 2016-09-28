55dd03d6eb0434a04373bd12ba5e887f0bcd7b43
Harry <arandomawesomegeek@gmail.com>
2016-09-28 10:40:05 -0400

    Update terminalApp.js

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -1,7 +1,7 @@
 var terminalApp = angular.module('terminalApp', []);
 
 terminalApp.controller('TerminalController', function TerminalController($scope, $sce){
-  $scope.command = "help";
+  $scope.command = "";
   $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
   $scope.update = function() {
     if($scope.command === "help") {

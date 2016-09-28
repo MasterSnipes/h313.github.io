b955860c1158a521fbbc303c0b53e865b05f8a1e
Harry <arandomawesomegeek@gmail.com>
2016-09-28 10:39:02 -0400

    Update terminalApp.js

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -5,12 +5,15 @@ terminalApp.controller('TerminalController', function TerminalController($scope,
   $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
   $scope.update = function() {
     if($scope.command === "help") {
+      console.log(1);
       $scope.output = $sce.trustAsHtml('<p><span class="green">ls</span></p>');
     }
     else if($scope.command === "ls") {
+      console.log(2);
       $scope.output = $sce.trustAsHtml('<p>projects</p>');
     }
     else if($scope.command === "cat projects") {
+      console.log(3);
       $scope.output = $sce.trustAsHtml('<p><a class="red" href="https://h313.github.io/KarlMarkov/">Karl Markov</a></p><p><a class="red" href="https://h313.github.io/SirMarkov/">Sir Markov</a></p>');
     }
     $scope.command = "";

1ac0f56e8b50e03eccd37e3aff6ec4c78f662d9d
Harry <arandomawesomegeek@gmail.com>
2016-09-27 20:49:37 -0400

    its not working but i have no idea why

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -1,9 +1,11 @@
 var terminalApp = angular.module('terminalApp', []);
 
-terminalApp.controller('TerminalController', function TerminalController($scope){
-  $scope.input = ">";
-  $scope.output = "";
-  $scope.update = function(){
-
+terminalApp.controller('TerminalController', function TerminalController($scope, $sce){
+  $scope.command = "help";
+  $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
+  $scope.update = function() {
+    if($scope.command == "help")
+      $scope.output = $sce.trustAsHtml("<p></p>");
+    $scope.command = "";
   };
 });

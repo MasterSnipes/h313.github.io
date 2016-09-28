0487dc7cf83e9beb8220adbdcfd73b410ee03d3f
Harry <arandomawesomegeek@gmail.com>
2016-09-27 20:59:51 -0400

    might fix the enter thing

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -4,12 +4,14 @@ terminalApp.controller('TerminalController', function TerminalController($scope,
   $scope.command = "help";
   $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
   $scope.update = function() {
-    if($scope.command == "help")
+    if (keyEvent.which === 13) {
+      if($scope.command == "help")
       $scope.output = $sce.trustAsHtml('<p><span class="green">ls</span></p>');
-    if($scope.command == "ls")
+      if($scope.command == "ls")
       $scope.output = $sce.trustAsHtml('<p>projects</p>');
-    if($scope.command == "cat projects")
+      if($scope.command == "cat projects")
       $scope.output = $sce.trustAsHtml('<p><a class="red" href="https://h313.github.io/KarlMarkov/">Karl Markov</a></p><p><a class="red" href="https://h313.github.io/SirMarkov/">Sir Markov</a></p>');
-    $scope.command = "";
+      $scope.command = "";
+    }
   };
 });

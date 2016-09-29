078c27aebc30157ce3a356a1620936d2d2751778
Harry <arandomawesomegeek@gmail.com>
2016-09-29 09:41:26 -0400

    more dependency

--- js/terminalApp.js
+++ js/terminalApp.js
@@ -1,6 +1,6 @@
 var terminalApp = angular.module('terminalApp', []);
 
-terminalApp.controller('TerminalController', function TerminalController($scope, $sce, $location){
+terminalApp.controller('TerminalController', function TerminalController($scope, $sce, $location, $anchorScroll){
   $scope.command = "help";
   $scope.output = $sce.trustAsHtml('<p>ARCH-4.7.1.1</p><p>Use <span class="green">help</span> for commands</p>');
   var loc = 0;
